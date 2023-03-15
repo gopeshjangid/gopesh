0c90f2419426fd94c0b8aa9a1c63d36903af8930
Gopesh Jangid <gopesh.jangid@spinnaker.co.uk>
2023-03-15 12:04:34 +0530

    fixed

--- src/data/header-data.js
+++ src/data/header-data.js
@@ -5,5 +5,5 @@ export const headerData = {
     desciption: "My name is GOPESH JANGID. I have 10 years experience in software development, Working as MERN stack developer. Developing Interactive and Progressive web applications in various javascript frameworks and libraries. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Have good expertise in various technologies ",
     image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/'
+    resumePdf: 'https://docs.google.com/document/d/e/2PACX-1vR2AvBKEJ_1R3rhv-YwwMPwCOntTpMeNFhH3IV97iku7wIndmV4nv5aIs5MpL-PcRxFvM5sj3cXbTsE/pub'
 }
